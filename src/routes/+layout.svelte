b2b6c250208276cf57e3d70a23e334e110e33db0
duhan <duhan@189.cn>
2026-01-25 15:04:48 +0800

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -105,7 +105,7 @@
 			reconnectionDelay: 1000,
 			reconnectionDelayMax: 5000,
 			randomizationFactor: 0.5,
-			path: `${base}/ws/socket.io`
+			path: `${base}/ws/socket.io`,
 			transports: enableWebsocket ? ['websocket'] : ['polling', 'websocket'],
 			auth: { token: localStorage.token }
 		});
